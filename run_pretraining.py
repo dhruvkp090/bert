c4b2ce0635d6872e58eb6c7a40b32ef08b698fdd
dhruvkp090 <45363265+dhruvkp090@users.noreply.github.com>
2019-05-20 15:26:20 +0530

    Update run_pretraining.py

--- run_pretraining.py
+++ run_pretraining.py
@@ -402,6 +402,22 @@ def _decode_record(record, name_to_features):
 
   return example
 
+def serving_input_fn():
+    with tf.variable_scope("foo"):
+    feature_spec = {
+        "input_ids": tf.FixedLenFeature([MAX_SEQ_LENGTH], tf.int64),
+        "input_mask": tf.FixedLenFeature([MAX_SEQ_LENGTH], tf.int64),
+        "segment_ids": tf.FixedLenFeature([MAX_SEQ_LENGTH], tf.int64),
+        "label_ids": tf.FixedLenFeature([], tf.int64),
+      }
+    serialized_tf_example = tf.placeholder(dtype=tf.string,
+                                           shape=[None],
+                                           name='input_example_tensor')
+    receiver_tensors = {'examples': serialized_tf_example}
+    features = tf.parse_example(serialized_tf_example, feature_spec)
+    return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
+
+
 
 def main(_):
   tf.logging.set_verbosity(tf.logging.INFO)
@@ -464,6 +480,10 @@ def main(_):
         max_predictions_per_seq=FLAGS.max_predictions_per_seq,
         is_training=True)
     estimator.train(input_fn=train_input_fn, max_steps=FLAGS.num_train_steps)
+    EXPORT_DIR = '/content'
+    estimator._export_to_tpu = False  # this is important
+    path = estimator.export_savedmodel(EXPORT_DIR, serving_input_fn)
+    print(path)
 
   if FLAGS.do_eval:
     tf.logging.info("***** Running evaluation *****")
@@ -477,7 +497,7 @@ def main(_):
 
     result = estimator.evaluate(
         input_fn=eval_input_fn, steps=FLAGS.max_eval_steps)
-
+    
     output_eval_file = os.path.join(FLAGS.output_dir, "eval_results.txt")
     with tf.gfile.GFile(output_eval_file, "w") as writer:
       tf.logging.info("***** Eval results *****")
